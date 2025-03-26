bbe9835c425b35885ec0abb5f9847e7b8eec4d62
akash patel <akash906129@gmail.com>
2025-03-26 18:06:51 +0530

    fixed some issue

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,11 @@
-import React, { FC, useMemo } from 'react';
+
 import { ConnectionProvider, WalletProvider } from '@solana/wallet-adapter-react';
-import { WalletAdapterNetwork } from '@solana/wallet-adapter-base';
-import { UnsafeBurnerWalletAdapter } from '@solana/wallet-adapter-wallets';
 import {
   WalletModalProvider,
   WalletDisconnectButton,
   WalletMultiButton,
-  WalletConnectButton
-} from '@solana/wallet-adapter-react-ui';
-import { clusterApiUrl } from '@solana/web3.js';
 
+} from '@solana/wallet-adapter-react-ui';
 
 import '@solana/wallet-adapter-react-ui/styles.css';
 import Airdrop from './components/Airdrop';
@@ -18,15 +14,13 @@ function App() {
 
 
   return (
-    <ConnectionProvider endpoint="https://solana-devnet.g.alchemy.com/v2/-HbMyyXskgweOHG97L3XM2Xx2zObO-Bz">
+    <ConnectionProvider endpoint="https://solana-devnet.core.chainstack.com/819be5176eed5a5d9249ab2905cd89ba">
       <WalletProvider wallets={[]} autoConnect>
         <WalletModalProvider>
 
           <WalletMultiButton />
           <WalletDisconnectButton />
-          <div>
-            hi there
-          </div>
+         
           <Airdrop></Airdrop>
         </WalletModalProvider>
       </WalletProvider>
