c54693c284ffa0adaedd19b6d512a5138bd76684
akash patel <akash906129@gmail.com>
2025-03-28 06:40:37 +0530

    components done

--- src/App.tsx
+++ src/App.tsx
@@ -6,22 +6,39 @@ import {
   WalletMultiButton,
 
 } from '@solana/wallet-adapter-react-ui';
-
+import { WalletAdapterNetwork } from '@solana/wallet-adapter-base';
 import '@solana/wallet-adapter-react-ui/styles.css';
 import Airdrop from './components/Airdrop';
-
+import { ShowBalance } from './components/ShowSolBalance';
+import { useMemo } from 'react';
+import { clusterApiUrl } from '@solana/web3.js';
+import { SignMessage } from './components/signMessage';
+import { SendTokens } from './components/sendToken';
 function App() {
 
-
+  const network = WalletAdapterNetwork.Devnet;
+  const endpoint = useMemo(() => clusterApiUrl(network), [network]);
   return (
-    <ConnectionProvider endpoint="https://solana-devnet.core.chainstack.com/819be5176eed5a5d9249ab2905cd89ba">
+    <ConnectionProvider endpoint={endpoint}>
       <WalletProvider wallets={[]} autoConnect>
         <WalletModalProvider>
 
-          <WalletMultiButton />
-          <WalletDisconnectButton />
-         
+          <div style={{ display: 'flex', justifyContent: 'space-between' }}>
+
+            <WalletMultiButton />
+            <WalletDisconnectButton />
+
+          </div>
+          <hr />
           <Airdrop></Airdrop>
+          <hr />
+          <ShowBalance />
+          <hr />
+          <SignMessage />
+          <hr />
+          <SendTokens />
+          <hr />
+
         </WalletModalProvider>
       </WalletProvider>
     </ConnectionProvider>
--- src/components/Airdrop.tsx
+++ src/components/Airdrop.tsx
@@ -1,35 +1,28 @@
 import { useConnection, useWallet } from '@solana/wallet-adapter-react';
 import { useState } from 'react';
-
+import { LAMPORTS_PER_SOL } from '@solana/web3.js';
 
 export default function Airdrop() {
     const wallet = useWallet();
     const { connection } = useConnection();
     const [amount, setAmount] = useState<number>(0);
-    async function sendAirdrop() {
+
+    async function requestAirdrop() {
         if (!wallet.publicKey) {
             console.log("no public key");
 
             return;
         }
+        await connection.requestAirdrop(wallet.publicKey, amount * LAMPORTS_PER_SOL)
+        alert("Airdropped " + amount + " SOL to " + wallet.publicKey.toBase58());
 
-        try {
-
-            const res = await connection.requestAirdrop(wallet.publicKey, amount * 10 ** 9);
-            console.log("res :", res);
-            alert("airdrop " + amount)
-
-        } catch (e) {
-
-            console.error(e);
-        }
     }
 
     return (
         <div>
             <input type="number" onChange={(e) => (setAmount(Number(e.target.value)))} placeholder="Enter amount" />
-            <button type="submit" onClick={sendAirdrop} > Send Airdrop</button>
- 
+            <button type="submit" onClick={requestAirdrop} > Request Airdrop</button>
+
 
         </div>
     )
